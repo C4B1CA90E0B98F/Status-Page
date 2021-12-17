98be42de2d0abdc13af54506d4860b58840d4217
C4B1CA90E0B98F <47320215+C4B1CA90E0B98F@users.noreply.github.com>
2021-12-17 09:47:49 +0700

    Initial commit

---
+++ check.sh
@@ -0,0 +1,61 @@
+# In the original repository we'll just print the result of status checks,
+# without committing. This avoids generating several commits that would make
+# later upstream merges messy for anyone who forked us.
+commit=true
+origin=$(git remote get-url origin)
+if [[ $origin == *CZ4B/Status-Page* ]]
+then
+  commit=false
+fi
+
+KEYSARRAY=()
+URLSARRAY=()
+
+urlsConfig="./urls.cfg"
+echo "Reading $urlsConfig"
+while read -r line
+do
+  echo "  $line"
+  IFS='=' read -ra TOKENS <<< "$line"
+  KEYSARRAY+=(${TOKENS[0]})
+  URLSARRAY+=(${TOKENS[1]})
+done < "$urlsConfig"
+
+mkdir -p logs
+
+for (( index=0; index < ${#KEYSARRAY[@]}; index++))
+do
+  key="${KEYSARRAY[index]}"
+  url="${URLSARRAY[index]}"
+  echo "  $key=$url"
+
+  for i in 1 2 3 4; 
+  do
+    response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
+    if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 307 ]; then
+      result="success"
+    else
+      result="failed"
+    fi
+    if [ "$result" = "success" ]; then
+      break
+    fi
+    sleep 5
+  done
+  dateTime=$(date +'%Y-%m-%d %H:%M')
+  if [[ $commit == true ]]
+  then
+    echo $dateTime, $result >> "logs/${key}_report.log"
+  else
+    echo "    $dateTime, $result"
+  fi
+done
+
+if [[ $commit == true ]]
+then
+  git config --global user.name $1
+  git config --global user.email $2
+  git add -A --force logs/
+  git commit -am 'Update Status'
+  git push
+fi
