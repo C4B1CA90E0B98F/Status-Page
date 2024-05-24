7eba3a859f7cafb6d3284587adb7fe5b996113eb
C4B1CA90E0B98F <dev@zero.biz.id>
2024-05-24 21:50:22 +0700

    Fixing check.sh

--- check.sh
+++ check.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 # In the original repository we'll just print the result of status checks,
 # without committing. This avoids generating several commits that would make
 # later upstream merges messy for anyone who forked us.
 commit=true
 origin=$(git remote get-url origin)
-if [[ $origin == *CZ4B/Status-Page* ]]
-then
+
+if [[ $origin == *CZ4B/Status-Page* ]]; then
   commit=false
 fi
 
@@ -13,49 +14,56 @@ URLSARRAY=()
 
 urlsConfig="./urls.cfg"
 echo "Reading $urlsConfig"
-while read -r line
-do
-  echo "  $line"
-  IFS='=' read -ra TOKENS <<< "$line"
-  KEYSARRAY+=(${TOKENS[0]})
-  URLSARRAY+=(${TOKENS[1]})
-done < "$urlsConfig"
+
+if [[ -f "$urlsConfig" ]]; then
+  while IFS='=' read -r key url || [[ -n $key ]]; do
+    key=$(echo "$key" | xargs)
+    url=$(echo "$url" | xargs)
+
+    if [[ -n "$key" && -n "$url" ]]; then
+      KEYSARRAY+=("$key")
+      URLSARRAY+=("$url")
+    fi
+  done < "$urlsConfig"
+else
+  echo "Error: $urlsConfig file not found."
+  exit 1
+fi
 
 mkdir -p logs
 
-for (( index=0; index < ${#KEYSARRAY[@]}; index++))
-do
+echo "Number of URLs: ${#URLSARRAY[@]}"
+
+for (( index=0; index < ${#KEYSARRAY[@]}; index++ )); do
   key="${KEYSARRAY[index]}"
   url="${URLSARRAY[index]}"
-  echo "  $key=$url"
 
-  for i in 1 2 3 4; 
-  do
-    response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
-    if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 307 ]; then
+  for i in {1..4}; do
+    echo "Checking $url (Attempt $i)"
+    response=$(curl --write-out '%{http_code}' --silent --output /dev/null "$url")
+
+    if [[ "$response" -eq 200 || "$response" -eq 202 || "$response" -eq 301 || "$response" -eq 307 ]]; then
       result="success"
+      break
     else
       result="failed"
     fi
-    if [ "$result" = "success" ]; then
-      break
-    fi
+
     sleep 5
   done
+
   dateTime=$(date +'%Y-%m-%d %H:%M')
-  if [[ $commit == true ]]
-  then
-    echo $dateTime, $result >> "logs/${key}_report.log"
-    echo "$(tail -2000 logs/${key}_report.log)" > "logs/${key}_report.log"
+
+  if [[ $commit == true ]]; then
+    echo "$dateTime, $result" >> "logs/${key}_report.log"
   else
     echo "    $dateTime, $result"
   fi
 done
 
-if [[ $commit == true ]]
-then
-  git config --global user.name $1
-  git config --global user.email $2
+if [[ $commit == true ]]; then
+  git config --global user.name "$1"
+  git config --global user.email "$2"
   git add -A --force logs/
   git commit -am '[Automated] Update Status'
   git push
